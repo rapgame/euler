a0b9aab981825c9f7fa7a56a870befeaeb6dd378
raprobe <Robert.Rap@rabobank.nl>
2020-03-08 20:10:12 +0100

    renamed files

--- Runall.py
+++ Runall.py
@@ -1,14 +1,14 @@
 #! /usr/bin/env python
 
 
-import Problem1
-import Problem2
+import problem1
+import problem2
 
 
 def main():
     """ the famous main running all solved problems"""
-    Problem1.main()
-    Problem2.main()
+    problem1.main()
+    problem2.main()
 
 
 if __name__ == "__main__":
